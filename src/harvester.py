005892f8e39f8a971834f5377ec56926d7ef997d
storm-sergey <st.sergey.a@gmail.com>
2021-02-12 16:09:19 +0300

    Update harvester.py

--- src/harvester.py
+++ src/harvester.py
@@ -1,4 +1,5 @@
 import time
+import json
 from datetime import datetime
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -8,50 +9,61 @@ from selenium.webdriver.support.ui import WebDriverWait
 CONTENT_CLASS_NAME = "content_3Gzc6maC"
 LINK_QUESTION_CLASS_NAME = "link_question_31Iopbs3"
 LINK_CATEGORY_CLASS_NAME = "link_category_lxzziIMq"
+questions = list()
 last_q_href = None
 counter = 1
 
 
 def harvest():
-    global last_q_href
+    global last_q_href, questions
+
+    with open("log.json", "w") as log_file:
+        json.dump({"questions": list()}, log_file, indent=2)
+
     with webdriver.Firefox() as driver:
         wait = WebDriverWait(driver, 1000)
         driver.get("https://otvet.mail.ru/")
         while True:
-            questions = driver.find_elements(By.CLASS_NAME, CONTENT_CLASS_NAME)
+            new_questions = driver.find_elements(By.CLASS_NAME, CONTENT_CLASS_NAME)
             if last_q_href is None:
-                last_q_href = questions[0].find_element(By.CLASS_NAME, LINK_QUESTION_CLASS_NAME).get_property('href')
+                last_q_href = new_questions[0].find_element(By.CLASS_NAME, LINK_QUESTION_CLASS_NAME).get_property('href')
             else:
-                found: bool = find_last_question(questions)
+                found: bool = find_last_question(new_questions)
                 while not found:
                     time.sleep(1)
                     driver.find_element(By.XPATH, '//span[contains(text(), "Показать еще")]').click()
-                    questions = driver.find_elements(By.CLASS_NAME, CONTENT_CLASS_NAME)
-                    found = find_last_question(questions)
+                    new_questions = driver.find_elements(By.CLASS_NAME, CONTENT_CLASS_NAME)
+                    found = find_last_question(new_questions)
 
-            f = open("log.txt", "a")
-            f.write(collect(questions))
-            f.close()
-            time.sleep(300)
+            add_to_log(new_questions)
+            time.sleep(60)
             driver.refresh()
 
 
-def collect(questions) -> str:
+def add_to_log(new_questions):
+    global questions
+    with open("log.json", "r") as log_file:
+        log = json.load(log_file)
+        questions = log["questions"]
+        questions.extend(collect(new_questions))
+    with open("log.json", "w") as log_file:
+        json.dump({"questions": questions}, log_file, indent=2)
+
+
+def collect(new_questions) -> list:
     global counter
-    c = ""
-    for q in reversed(questions):
-        c += "id: " + str(counter) + "\n"
-        # question
-        c += "question: \"" + q.find_element(By.CLASS_NAME, LINK_QUESTION_CLASS_NAME).text + "\"\n"
-        c += "question link: " + q.find_element(By.CLASS_NAME, LINK_QUESTION_CLASS_NAME).get_property('href') + "\n"
-        # category
-        c += "category: \"" + q.find_element(By.CLASS_NAME, LINK_CATEGORY_CLASS_NAME).text + "\"\n"
-        c += "category link: " + q.find_element(By.CLASS_NAME, LINK_CATEGORY_CLASS_NAME).get_property('href') + "\n"
-        # date
-        c += "timestamp: " + datetime.now().strftime("%d/%m/%Y %H:%M:%S") + "\n"
-        c += "\n"
+    question_list = list()
+    for q in reversed(new_questions):
+        question = dict()
+        question["id"] = str(counter)
+        question["question"] = q.find_element(By.CLASS_NAME, LINK_QUESTION_CLASS_NAME).text
+        question["question link"] = q.find_element(By.CLASS_NAME, LINK_QUESTION_CLASS_NAME).get_property('href')
+        question["category"] = q.find_element(By.CLASS_NAME, LINK_CATEGORY_CLASS_NAME).text
+        question["category link"] = q.find_element(By.CLASS_NAME, LINK_CATEGORY_CLASS_NAME).get_property('href')
+        question["timestamp"] = datetime.now().strftime("%d.%m.%Y %H:%M:%S")
+        question_list.append(question)
         counter += 1
-    return c
+    return question_list
 
 
 def find_last_question(qs):
@@ -70,7 +82,7 @@ def find_all_question(d):
 
 
 def check_class_names():
-    """mock"""
+    """a mock"""
     return None
 
 

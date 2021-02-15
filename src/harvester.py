d458d01b4fda440e44e3862e926090732ab496ed
storm-sergey <st.sergey.a@gmail.com>
2021-02-15 13:04:48 +0300

    add category reading and timestamp

--- src/harvester.py
+++ src/harvester.py
@@ -1,55 +1,78 @@
 import time
+from datetime import datetime
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 
+
+CONTENT_CLASS_NAME = "content_3Gzc6maC"
+LINK_QUESTION_CLASS_NAME = "link_question_31Iopbs3"
+LINK_CATEGORY_CLASS_NAME = "link_category_lxzziIMq"
 last_q_href = None
+counter = 1
 
 
-def find_all_question(_webdr):
-    return _webdr.find_elements(By.XPATH, '//a[contains(@class, "link_question_")]')
+def harvest():
+    global last_q_href
+    with webdriver.Firefox() as driver:
+        wait = WebDriverWait(driver, 1000)
+        driver.get("https://otvet.mail.ru/")
+        while True:
+            questions = driver.find_elements(By.CLASS_NAME, CONTENT_CLASS_NAME)
+            if last_q_href is None:
+                last_q_href = questions[0].find_element(By.CLASS_NAME, LINK_QUESTION_CLASS_NAME).get_property('href')
+            else:
+                found: bool = find_last_question(questions)
+                while not found:
+                    time.sleep(1)
+                    driver.find_element(By.XPATH, '//span[contains(text(), "Показать еще")]').click()
+                    questions = driver.find_elements(By.CLASS_NAME, CONTENT_CLASS_NAME)
+                    found = find_last_question(questions)
+
+            f = open("log.txt", "a")
+            f.write(collect(questions))
+            f.close()
+            time.sleep(300)
+            driver.refresh()
 
 
-def show_more_questions(_webdr):
-    _webdr.find_element(By.XPATH, '//span[contains(text(), "Показать еще")]').click()
+def collect(questions) -> str:
+    global counter
+    c = ""
+    for q in reversed(questions):
+        c += "id: " + str(counter) + "\n"
+        # question
+        c += "question: \"" + q.find_element(By.CLASS_NAME, LINK_QUESTION_CLASS_NAME).text + "\"\n"
+        c += "question link: " + q.find_element(By.CLASS_NAME, LINK_QUESTION_CLASS_NAME).get_property('href') + "\n"
+        # category
+        c += "category: \"" + q.find_element(By.CLASS_NAME, LINK_CATEGORY_CLASS_NAME).text + "\"\n"
+        c += "category link: " + q.find_element(By.CLASS_NAME, LINK_CATEGORY_CLASS_NAME).get_property('href') + "\n"
+        # date
+        c += "timestamp: " + datetime.now().strftime("%d/%m/%Y %H:%M:%S") + "\n"
+        c += "\n"
+        counter += 1
+    return c
 
 
 def find_last_question(qs):
     global last_q_href
     for i in range(len(qs)):
-        if qs[i].get_property('href') == last_q_href:
-            last_q_href = qs[0].get_property('href')
+        if qs[i].find_element(By.CLASS_NAME, LINK_QUESTION_CLASS_NAME).get_property('href') == last_q_href:
+            last_q_href = qs[0].find_element(By.CLASS_NAME, LINK_QUESTION_CLASS_NAME).get_property('href')
             qs[:] = qs[:i]
             return True
     return False
 
 
-def delay(d=1):
-    time.sleep(d)
+def find_all_question(d):
+    """this function is saved for the class names checking in the future feature"""
+    return d.find_elements(By.XPATH, '//a[contains(@class, "link_question_")]')
 
 
-with webdriver.Firefox() as driver:
-    counter = 1
-    wait = WebDriverWait(driver, 1000)
-    driver.get("https://otvet.mail.ru/")
+def check_class_names():
+    """mock"""
+    return None
 
-    while True:  # сделай ограничение по дате
-        questions = find_all_question(driver)
-        if last_q_href is None:
-            last_q_href = questions[0].get_property('href')
-        else:
-            found: bool = find_last_question(questions)
-            while not found:
-                delay(1)
-                show_more_questions(driver)
-                questions = find_all_question(driver)
-                found = find_last_question(questions)
 
-        f = open("log.txt", "a")
-        for q in reversed(questions):
-            f.write(str(counter) + ": " + q.get_property('href') + "\n[\"" + q.text + "\"]\n\n")
-            counter += 1
-
-        f.close()
-        delay()
-        driver.refresh()
+if __name__ == "__main__":
+    harvest()
